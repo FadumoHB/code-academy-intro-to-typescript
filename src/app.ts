5b4de5df255e8439f622641ada9a71c239ab810c
FadumoHB <fhbotan1@gmail.com>
2021-08-23 13:33:19 +0000

    Basics

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,36 @@
-console.log('hello,world');
+import { personGreeter } from "./personGreeter";
+
+const firstName = "Fadumo"; 
+const secondName = "Botan"; 
+
+const fullName = '${firstName + secondName}' ; 
+
+console.log('Hello ${firstName} ${secondName}') 
+
+type Person = { 
+    firstName: string;
+    secondName: string; 
+    dateOfBirth?: Date; 
+  
+};
+
+const person : Person = { 
+    firstName: 'Fadumo',
+    secondName: 'Botan',  
+    dateOfBirth: new Date (1992, 6, 30) 
+
+}; 
+
+const sayHelloToPerson = (p: Person) => {
+    console.log ('Hello ${p.firstName} ${p.secondName}')
+
+}
+sayHelloToPerson(person); 
+
+const greeter = new personGreeter("", "");
+greeter.greet();
+
+
+
+
+
